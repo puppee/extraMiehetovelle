a71f6b8bb7990ac20a43935df03cf4728e662b01
Tuomas <tuomas@puppe.me>
2016-08-08 21:55:50 +0300

    E
    Added new login & removed loginconfig

--- loginconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'vaihda');
-   define('DB_PASSWORD', 'vaihda');
-   define('DB_DATABASE', 'vaihda');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
