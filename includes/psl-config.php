4787f7a0295df27fbd40eb597ff587673dc73ec1
Tuomas <tuomas@puppe.me>
2016-08-08 22:15:09 +0300

    E
    Login editing, once again.

---
+++ includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "miehetovelle");    // The database username. 
+define("PASSWORD", "passwordformiehetovelle");    // The database password. 
+define("DATABASE", "miehetovelle");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
