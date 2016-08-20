db83c4d7e51cbd18f96b5c1f5ff79476b3b7727e
puppee <pupp3@icloud.com>
2016-08-20 17:52:58 +0300

    Update psl-config.php

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -1,14 +1,11 @@
 <?php
-/**
- * These are the database login details
- */  
-define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "miehetovelle");    // The database username. 
-define("PASSWORD", "passwordformiehetovelle");    // The database password. 
-define("DATABASE", "miehetovelle");    // The database name.
+define("HOST", "localhost");     // Localhost
+define("USER", "miehetovelle");    // Tietokannan käyttäjä
+define("PASSWORD", "passwordformiehetovelle");    // Tietokannan salasana
+define("DATABASE", "miehetovelle");    // Tietokannan nimi
  
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
  
-define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+define("SECURE", FALSE);    // Devin jälkeen ssl päälle
 ?>
