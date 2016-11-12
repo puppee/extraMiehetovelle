3bb8b58e1b34047f8b107f349bf4847ea9060219
Tuomas <tuomas@puppe.me>
2016-11-12 22:55:23 +0200

    AdminLTE 2.3.7

--- Build/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'miehetovelle');
-   define('DB_PASSWORD', 'passwordformiehetovelle');
-   define('DB_DATABASE', 'miehetovelle');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
